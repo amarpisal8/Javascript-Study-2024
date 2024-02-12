25943e41fda5956ef6c5a9910fb55cfa813b803a
Amar Pisal <109473825+amarpisal8@users.noreply.github.com>
2024-02-12 01:38:48 +0000

    Day 3

---
+++ String.js
@@ -0,0 +1,21 @@
+let name = "Amar";
+let Surname = "Pisal"
+
+let employee ={
+    Name : "Amar Pisal",
+    Id : "123223",
+    Designation : "Software Developer"
+}
+
+console.log(`My First Name is ${name} and my surname is ${Surname}`);
+console.log(`Employee of the year goes to ${employee.Name} and him Employeement id is ${employee.Id} and him Designation is ${employee.Designation}`);
+
+let Name = ["Joe","Jon","Jack","Jenny","Marry"];
+let RollNumber = [1,2,3,4,5];
+const Marks = [92,89,97,92,85]
+for(let i=0; i<=5; i++){
+    // console.log(`Student Name Is ${Name[i]} and its Roll Number is ${RollNumber[i]}`);
+    console.log(`${RollNumber[i]}  ${Name[i]} ${Marks[i]}`)
+}
+
+
