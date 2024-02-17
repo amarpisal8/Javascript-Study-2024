a9a1313b71ec9c46dffd465f38ff6f55551a2c39
Amar Pisal <109473825+amarpisal8@users.noreply.github.com>
2024-02-17 10:36:27 +0000

    Day 6

---
+++ Dice.js
@@ -0,0 +1,9 @@
+// Function to roll a six-sided dice and print the result
+function rollDice() {
+    var diceResult = Math.floor(Math.random() * 6) + 1;
+    console.log("Dice result: " + diceResult);
+}
+
+// Call the function to roll the dice
+rollDice();
+
