133258d22a11f4b957e0797b0023a9ef838c9189
Amar Pisal <109473825+amarpisal8@users.noreply.github.com>
2024-02-17 10:12:04 +0000

    Day 6

---
+++ Maths.js
@@ -0,0 +1,4 @@
+console.log(Math.abs(-2))           //Get Opposite Number   (-2 == 2)
+console.log(Math.round(2.3));       //Get RoundOff number   (2.3 == 2)
+console.log(Math.ceil(2.3));        //Get Ceil Number       (2.3 == 3)
+console.log(Math.floor(4.9));       //returns the largest integer less than or equal to a given number. (4)
---
+++ NumberJavascript.js
@@ -0,0 +1,12 @@
+let score = 400
+console.log(score);
+const Balance = new Number(100);
+console.log(Balance);
+
+//method
+console.log(Balance.toString.length);       //converted to string and get length
+console.log(Balance.toFixed(3))             //Get decimal number eg. 100 tu 100.000
+const otherNumber = 12.64345;
+console.log(otherNumber.toPrecision(2));     //Get Precise value i/p under bracker eg You Enter Pre Val 2 Number is 32.343  output will be 33
+const पैसा = 1200000000000000000000;
+console.log(पैसा.toLocaleString('en-IN'))
